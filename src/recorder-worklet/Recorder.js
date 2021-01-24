3dbfb6b67aebe91f5f57c6429765c37c5d60985a
samstorment <sjstorment22@gmail.com>
2021-01-23 20:37:56 -0600

    Removed timestamp comments

--- src/recorder-worklet/Recorder.js
+++ src/recorder-worklet/Recorder.js
@@ -41,8 +41,6 @@ export const Recorder = () => {
         rec.current.start()
         .then(() => {
 
-            console.log(Tone.now());
-
             setRecording(true);
             startTracks();
 
